45e9f8435d37d6ae725b3176baa0d902f5399be4
yerin <sky970312@naver.com>
2026-01-19 14:57:19 +0900

    초기화

---
+++ prettier.config.ts
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    singleQuote: false,
+    trailingComma: "all",
+    tabWidth: 4,
+    printWidth: 100,
+    arrowParens: "avoid",
+    bracketSameLine: true,
+};
+
+export default config;
